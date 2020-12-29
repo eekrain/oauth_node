d40b7fa66533ce8d4a4a9856544dec4d9878b057
Ardian Eka Candra <ardianoption10@gmail.com>
2020-12-29 15:33:47 +0700

    done cookie session

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require("express");
 require("dotenv").config();
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 
 const authRoutes = require("./routes/auth-routes");
 const passportSetup = require("./config/passport-setup");
-const mongoose = require("mongoose");
 
 const app = express();
 
 // set view engine
 app.set("view engine", "ejs");
+
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [process.env.cookieKey],
+  })
+);
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose
   .connect(process.env.mongodbURI, {
