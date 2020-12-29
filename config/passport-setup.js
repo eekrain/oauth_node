bc6376b4579987dc070014ce6682adc828afd577
Ardian Eka Candra <ardianoption10@gmail.com>
2020-12-29 12:19:55 +0700

    selesai part 6 menggunakan google+ api

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -4,6 +4,9 @@ const GoogleStrategy = require("passport-google-oauth20");
 passport.use(
   new GoogleStrategy({
     // options for the google strat
+    clientID:
+      "449418706030-rg79dpmr7oc2okpqu083n53tvm8ruhsg.apps.googleusercontent.com",
+    clientSecret: "l4kRm-JGgOhlZovSjE-GzGGV",
   }),
   () => {
     // passport callback fn
