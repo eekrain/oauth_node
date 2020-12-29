e4300d78ce89d4d3e8468f4e653d9d41cde5ff5b
Ardian Eka Candra <ardianoption10@gmail.com>
2020-12-29 13:53:57 +0700

    membuat instance mongo atlas + koneksi menggunakan mongoose

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
+require("dotenv").config();
 
 const authRoutes = require("./routes/auth-routes");
 const passportSetup = require("./config/passport-setup");
+const mongoose = require("mongoose");
 
 const app = express();
 
 // set view engine
 app.set("view engine", "ejs");
+
+// connect to mongodb
+mongoose.connect(process.env.mongodbURI, () => {
+  console.log("connected mongodb");
+});
 
 // auth routes
 app.use("/auth", authRoutes);
